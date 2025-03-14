b50eb80700e2ccf5be594b2ec91bf40ed8a22585
Leaf Ying <leaf.ying.work@gmail.com>
2025-03-15 02:27:17 +0800

    Copy test_crypto program to rootfs

--- lab2/make_rootfs.sh
+++ lab2/make_rootfs.sh
@@ -4,14 +4,17 @@ set -eu
 pwd=$PWD
 
 # unzip rootfs
-cd vm/dist && rm -rf rootfs && mkdir rootfs && cd rootfs && (bunzip2 -c ../rootfs.cpio.bz2 | cpio -i) && cd "$pwd"
+cd vm/dist && rm -rf rootfs && mkdir rootfs && cd rootfs && (bunzip2 -c ../rootfs.cpio.bz2 | cpio -i) && cd "${pwd}"
 
 # copy built module to rootfs
-cd cryptomod && make clean && make && make install && cd "$pwd"
+cd cryptomod && make clean && make && make install && cd "${pwd}"
 
 # copy installation script to rootfs
 chmod +x install_module.sh
 cp install_module.sh vm/dist/rootfs
 
+# copy test_crypto to rootfs
+cd vm && chmod +x test_crypto && cp test_crypto dist/rootfs && cd "${pwd}"
+
 # zip rootfs
-cd vm/dist && cd rootfs && (find . | cpio -o --format newc | bzip2 >../rootfs.cpio.bz2) && cd "$pwd"
+cd vm/dist && cd rootfs && (find . | cpio -o --format newc | bzip2 >../rootfs.cpio.bz2) && cd "${pwd}"
