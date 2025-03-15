75035879a0ed0c1981553a4a0a2d7fd793b8dc83
Leaf Ying <leaf.ying.work@gmail.com>
2025-03-15 22:08:26 +0800

    Add more test cases to install_module.sh

--- lab2/install_module.sh
+++ lab2/install_module.sh
@@ -4,8 +4,31 @@ set -eu
 # install all modules in /modules
 find /modules -name "*.ko" -type f -exec "insmod" "{}" \;
 
+# download missing files
+echo "6a98b2ed0c47f631e3b8995db9dffb29  test_crypto" >>/md5sum
+echo "6bcfed610a256de36acdf4eb106f58b7  fun.jpg.enc" >>/md5sum
+
+if [ ! -f "/test_crypto" ]; then
+	echo "Downloading test_crypto..."
+	wget https://up.zoolab.org/unixprog/lab02/test_crypto
+fi
+
+if [ ! -f "/fun.jpg.enc" ]; then
+	echo "Downloading fun.jpg.enc..."
+	wget https://up.zoolab.org/unixprog/lab02/fun.jpg.enc
+fi
+
+md5sum -c /md5sum
+chmod +x /test_crypto
+
+# tests
+echo 4 >/proc/sys/kernel/printk
+
 i=0
 while [ "${i}" -le 6 ]; do
-	./test_crypto test "${i}"
+	/test_crypto test "${i}"
 	i=$((i + 1))
 done
+/test_crypto dec -i fun.jpg.enc -o fun.jpg -k "e381aae38293e381a7e698a5e697a5e5bdb1e38284e381a3e3819fe381ae213f" -s 128 -m ADV
+
+echo 8 >/proc/sys/kernel/printk
