9f01761a48baee823cb6b78907b9bcd631ef1024
guyleaf <leaf.ying.work@gmail.com>
2025-03-17 18:52:55 +0800

    Update checksum for test_crypto

--- lab2/install_module.sh
+++ lab2/install_module.sh
@@ -5,7 +5,7 @@ set -eu
 find /modules -name "*.ko" -type f -exec "insmod" "{}" \;
 
 # download missing files
-echo "6a98b2ed0c47f631e3b8995db9dffb29  test_crypto" >>/md5sum
+echo "4d8af083e4c73a721a13957232ddb056  test_crypto" >/md5sum
 echo "6bcfed610a256de36acdf4eb106f58b7  fun.jpg.enc" >>/md5sum
 
 if [ ! -f "/test_crypto" ]; then
