edf822d6def4a51f1c365803e151fa02954534aa
Leaf Ying <leaf.ying.work@gmail.com>
2025-04-02 15:01:04 +0800

    Silent during generating got header

--- lab3/tools/generate_got_header.py
+++ lab3/tools/generate_got_header.py
@@ -2,6 +2,7 @@ import argparse
 from pathlib import Path
 
 from pwn import ELF
+from pwnlib.args import SILENT
 
 
 def parse_args():
@@ -25,6 +26,8 @@ if __name__ == "__main__":
     out_path = Path(args.output)
     out_path.parent.mkdir(parents=True, exist_ok=True)
 
+    # read ELF in silent mode
+    SILENT("")
     elf = ELF(args.path)
     header_name = out_path.name.upper().replace(".", "_")
     header_name = f"__{header_name}__"
@@ -32,16 +35,16 @@ if __name__ == "__main__":
         f.write(f"#ifndef {header_name}\n")
         f.write(f"#define {header_name}\n\n")
 
-        main_addr = hex(elf.symbols["main"])
-        f.write(f"#define MAIN {main_addr}\n\n")
+        main_addr = elf.symbols["main"]
+        f.write(f"#define MAIN {hex(main_addr)}\n\n")
 
-        print("main =", main_addr)
-        print("{:<12s} {:<10s} {:<10s}".format("Func", "GOT Offset", "Symbol Offset"))
+        # print("main =", main_addr)
+        # print("{:<12s} {:<10s} {:<10s}".format("Func", "GOT Offset", "Symbol Offset"))
         for s in [f"gop_{i+1}" for i in range(1200)]:
             if s in elf.got:
                 got_addr = elf.got[s]
                 f.write(f"#define {s.upper()} {hex(got_addr)}\n")
 
-                print("{:<12s} {:<10x} {:<10x}".format(s, got_addr, elf.symbols[s]))
+                # print("{:<12s} {:<10x} {:<10x}".format(s, got_addr, elf.symbols[s]))
 
         f.write("\n#endif\n")
